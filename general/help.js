c1cbd2f61c56909f243fea44532eab33575a6b86
MHBOL <romeodzromeodz@gmail.com>
2025-05-21 09:45:00 +0100

    Update help.js

--- general/help.js
+++ general/help.js
@@ -14,7 +14,7 @@ if(!args[0]) {
 	const embed1 = new Discord.MessageEmbed()
 	.setColor('RANDOM')
 	.setTitle('🚀 **HELP** 🚀')
-	.setDescription("**DDOS ATTACK BY VenThings** \n `.admin` \n `.listadmin` \n**Don't attack .gov.edu.fbi** \n `.methods` \n `.help` \n **Developer : VenThings**")
+	.setDescription("**DDOS ATTACK BY LIZNDYA** \n `.admin` \n `.listadmin` \n**Don't attack .gov.edu.fbi** \n `.methods` \n `.help` \n **Developer : ROMEO**")
 	message.channel.send(embed1);
 	return;
 	}
