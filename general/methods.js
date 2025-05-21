a6b9bc52daf45a851402ff083154892e95285a18
MHBOL <romeodzromeodz@gmail.com>
2025-05-21 09:43:49 +0100

    Update methods.js

--- general/methods.js
+++ general/methods.js
@@ -14,7 +14,7 @@ if(!args[0]) {
 	const embed1 = new Discord.MessageEmbed()
 	.setColor('RANDOM')
 	.setTitle('🚀 **BOTNET-PANEL** 🚀')
-  .setDescription("**FIREWALL** \n `CFB`  `CF-UAM`  `CF-TLS` \n **LAYER 7** \n `IDK`  `HULK`  `CFSockets`  `BYPASS`  `HTTP-V2`  `HTTP-GET`  `TLS-STRONG`  `TLSV1`  `HTTP-FLOOD`  `HTTP-BY`  `HTTP-VIP`  `HTTP-RAW`  `HTTP-RAND`  `HTTPS-GO`  `HTTP-GO`  \n **LAYER 4** \n `TCP-KILL` \n `OVH` \n `SAMP` \n `ven100gbps` \n **TOOLS** \n `geoip` \n `numberlookup` \n `mcbot` \n **Free new methods **")
+  .setDescription("**FIREWALL** \n `CFB`  `CF-UAM`  `CF-TLS` \n **LAYER 7** \n `IDK`  `HULK`  `CFSockets`  `BYPASS`  `HTTP-V2`  `HTTP-GET`  `TLS-STRONG`  `TLSV1`  `HTTP-FLOOD`  `HTTP-BY`  `HTTP-VIP`  `HTTP-RAW`  `HTTP-RAND`  `HTTPS-GO`  `HTTP-GO`  \n **LAYER 4** \n `TCP-KILL` \n `OVH` \n `SAMP` \n `ven100gbps` \n **TOOLS** \n `geoip` \n `numberlookup` \n `mcbot` \n **VIP new methods **")
 	message.channel.send(embed1);
 	return;
 	}
